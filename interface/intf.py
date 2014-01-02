1cb5cdb609aad90813fbed556207d82754baa282
Nipun <nipunjaswal@rocketmail.com>
2014-01-03 00:34:39 +0530

    Directory Structure

---
+++ interface/intf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# WFF Interface Design
+# Created By Nipun Jaswal
+# Email : mail@nipunjaswal.info
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+def style():
+	Name=  "#         Wireless Forensics Framework 0.1v       #"
+	Author=  "#          Developer : Nipun Jaswal(Ap3x)         #"
+	print  "###################################################"
+	print  "###################################################"
+	print  "###################################################"
+	print (Back.RED + Name + Style.RESET_ALL)
+	print (Back.RED + Author + Style.RESET_ALL)
+	print  "###################################################"
+	print  "###################################################"
+	print  "###################################################"
+	
