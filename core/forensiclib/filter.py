ffbe65abe9f173fa7fd72154750d5597574c2a80
Nipun <nipunjaswal@rocketmail.com>
2014-01-18 01:59:40 +0530

    Colorization Rules

--- core/forensiclib/filter.py
+++ core/forensiclib/filter.py
@@ -5,6 +5,9 @@
 # Email : mail@nipunjaswal.info
 from conf import notation
 import os
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
 def show_all_destination():
 	command=notation.libc+ " -r "+notation.pack_file+notation.sortd
 	print("Destinations")
@@ -45,13 +48,25 @@ def show_high_data():
 	line=execute.split("\n")
 	j=len(line)
 	j=j-2
+	a=j
+	max_bit=0
+	int(max_bit)
 	print("No.of Packets\tSource\t\t\tDestination")
 	print("=============\t======\t\t\t===========")
 	while(j>=0):
                 k=line[j]
                 bits,source,dest=k.split(",")
-          	print bits+"\t\t"+source+"\t"+dest
-                j=j-1
+		if(int(bits)>max_bit):
+			max_bit=int(bits)
+		j=j-1
+	while(a>=0):
+		b=line[a]
+		bits1,source1,dest1=b.split(",")
+                if(int(bits1)==max_bit):
+			print(Fore.RED+Back.BLACK+bits1+"\t\t"+source1+"\t"+dest1+Style.RESET_ALL)
+		else:
+                	print bits1+"\t\t"+source1+"\t"+dest1
+		a=a-1
 	
 
 
