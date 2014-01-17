b6f76208be48060213d92bbee8f4df0b1b568f4c
Nipun <nipunjaswal@rocketmail.com>
2014-01-17 19:16:32 +0530

    Data Mgmt Variables

--- conf/notation.py
+++ conf/notation.py
@@ -11,13 +11,17 @@ libc="tshark"
 #Current File Loaded
 pack_file="~/deepak-01.cap"	
 #===========================================
-#Filter Variables
+#Filter Variables Mgmt Frames
 authentications="wlan.fc.type_subtype==0x0B"
 associations_req="wlan.fc.type_subtype==0x00"
 deauth="wlan.fc.type_subtype==0x0C"
+#==========================================
+#Filter Variables Data Frames
+data="wlan.fc.type==2"
 #===========================================
 #Sorting and Unique Functions
 sorts=" -T fields -e wlan.sa | sort | uniq"
 sortd=" -T fields -e wlan.da | sort | uniq"
 sort_source_dest=" -T fields -E separator=, -e wlan.da -e wlan.sa | sort| uniq"
+sort_data_count=" -T fields -E separator=, -e wlan.da -e wlan.sa |sort | uniq -c | sed 's/,//'"
 #===========================================
