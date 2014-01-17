8721c26386f6e6a4acef5d258245eeb09f3e1f86
Nipun <nipunjaswal@rocketmail.com>
2014-01-17 15:57:41 +0530

    Removed Local Variables

--- core/forensiclib/filter.py
+++ core/forensiclib/filter.py
@@ -5,15 +5,8 @@
 # Email : mail@nipunjaswal.info
 from conf import notation
 import os
-pcap_file=notation.pack_file
-libcap_var=notation.libc
-auth_pack_filter=notation.authentications
-sort_source=notation.sorts
-sort_dest=notation.sortd
-assoc_req=notation.associations_req
-sort_source_dest=notation.sort_source_dest
 def show_all_destination():
-	command=libcap_var+ " -r "+pcap_file+sort_dest
+	command=notation.libc+ " -r "+notation.pack_file+notation.sortd
 	print("Destinations")
 	print("============")
 	execute=os.popen(command).read()
@@ -21,12 +14,12 @@ def show_all_destination():
 def show_all_source():
 	print("Sources")
         print("=======")
-	command=libcap_var+ " -r "+pcap_file+sort_source
+	command=notation.libc+ " -r "+notation.pack_file+notation.sorts
 	execute=os.popen(command).read()
 	print(execute)
 
 def show_all_association_requests():
-	command=libcap_var+ " -r "+pcap_file+" -R "+assoc_req + sort_source_dest
+	command=notation.libc+ " -r "+notation.pack_file+" -R "+notation.associations_req + notation.sort_source_dest
 	execute=os.popen(command).read()
 	source,dest=execute.split(",")
 	print"Source"+"\t\t\t\t\t\t"+"Destination"
