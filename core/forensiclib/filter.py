973ee9594dfed82a3a8c3227df61faa1f23a62c5
Nipun <nipunjaswal@rocketmail.com>
2014-01-17 18:20:32 +0530

    Fixed Iterations

--- core/forensiclib/filter.py
+++ core/forensiclib/filter.py
@@ -21,15 +21,28 @@ def show_all_source():
 def show_all_association_requests():
 	command=notation.libc+ " -r "+notation.pack_file+" -R "+notation.associations_req + notation.sort_source_dest
 	execute=os.popen(command).read()
+	print execute
 	source,dest=execute.split(",")
 	print"Source"+"\t\t\t\t\t\t"+"Destination"
-	print source+"\t\t\t\t"+dest
+	print source+"\t\t====>\t\t"+dest
 	
 def show_all_Deauth():
-        command=notation.libc+ " -r "+notation.pack_file+" -R "+notation.deauth + notation.sort_source_dest
+        command=notation.libc+ " -r "+notation.pack_file+" -R "+notation.deauth+notation.sort_source_dest
         execute=os.popen(command).read()
-        source,dest=execute.split(",")
-        print"Source"+"\t\t\t\t\t\t"+"Destination"
-        print source+"\t\t\t\t"+dest
+	line=execute.split("\n")
+	j=len(line)
+	j=j-2
+    	print"Source"+"\t\t\t\t\t\t"+"Destination"
+	while(j>=0):
+		k=line[j]
+		source,dest=k.split(",")
+        	print source+"\t\t====>\t\t"+dest
+		j=j-1
+
+
+		
+
+
+
 
 	
