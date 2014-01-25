45c821f51d955576a60f62c21016c852d094054e
Nipun <nipunjaswal@rocketmail.com>
2014-01-25 21:41:49 +0530

    Auto Byte Code Remover Added

---
+++ cleanup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# WFF Interface Design
+# Created By Nipun Jaswal
+# Email : mail@nipunjaswal.info
+from conf import notation
+import os
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+os.system("rm conf/*.pyc")
+os.system("rm core/forensiclib/*.pyc")
+os.system("rm interface/*.pyc")
+os.system("rm modules/*.pyc")
+os.system("rm core/*.pyc")
+
+
+	
+
+
+
+
+	
